b73bd69c5b8e19b74bef9a60b7394feb0da4e4b3
Huaxing YUAN <huaxing.yuan@axa.fr>
2023-07-04 18:02:34 +0200

    first commit

---
+++ src/Axe.HtmlReport/HtmlReportBuilder.cs
@@ -0,0 +1,76 @@
+﻿using Deque.AxeCore.Commons;
+using Microsoft.Playwright;
+using OpenQA.Selenium;
+
+namespace Axe.HtmlReport
+{
+    /// <summary>
+    /// Classes for generating HTML report from given Axe-Core results
+    /// </summary>
+    public class HtmlReportBuilder
+    {
+        public HtmlReportOptions Options { get; set; }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlReportBuilder"/> class using default options.
+        /// </summary>
+        public HtmlReportBuilder()
+        {
+            Options = new HtmlReportOptions()
+            {
+                OutputFolder = null,
+                OutputFormat = OutputFormat.Html,
+                UseAdvancedScreenshot = true
+            };
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlReportBuilder"/> class using the specified options.
+        /// </summary>
+        /// <param name="options"></param>
+        public HtmlReportBuilder(HtmlReportOptions options)
+        {
+            Options = options;
+        }
+
+        /// <summary>
+        /// The object holding the WebDriver instance.
+        /// </summary>
+        private object? driver;
+
+        /// <summary>
+        /// Use Selenium WebDriver
+        /// </summary>
+        /// <param name="driver">WebDriver object</param>
+        /// <returns></returns>
+        public HtmlReportBuilder WithDriver(IWebDriver driver)
+        {
+            this.driver = driver;
+            return this;
+        }
+
+        /// <summary>
+        /// Run Axe-Core analysis and returns analysis results
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
+        public AxeResult Analyze()
+        {
+            if(this.driver is IWebDriver webDriver)
+            {
+               Deque.AxeCore.Selenium.AxeBuilder axeBuilder = new Deque.AxeCore.Selenium.AxeBuilder(webDriver);
+               var results = axeBuilder.Analyze();
+               return results;
+            }
+            else
+            {
+               
+               throw new System.Exception("No WebDriver is provided found.");
+            }
+        }
+
+        public string ConvertToHtml(AxeResult result)
+        {
+        }
+    }
+}
---
+++ src/Axe.HtmlReport/HtmlReportOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axe.HtmlReport
+{
+    /// <summary>
+    /// Options of generating HTML report
+    /// </summary>
+    public sealed class HtmlReportOptions
+    {
+        /// <summary>
+        /// Gets or sets the output format of the HTML report: Zip Archive or a folder containing HTML report and all resources.
+        /// </summary>
+        public OutputFormat OutputFormat { get; set; } = OutputFormat.Html;
+    
+        /// <summary>
+        /// Gets or sets the output folder of the HTML report. If not set, the report will be generated in default temprary folder.
+        /// </summary>
+        public string? OutputFolder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value indicating if advanced screenshot should be used. Default is true.
+        /// </summary>
+        /// <remarks>
+        /// Advanced screenshot is a screenshot of the whole viewport, and the element in question is highlighted.
+        /// Otherwise, a screenshot of the element in question is taken.
+        /// Advanced screenshot marks the element in question more clearly, but it takes more space (images are larger).
+        /// </remarks>
+        public bool UseAdvancedScreenshot { get; set; } = true;
+    }
+}
